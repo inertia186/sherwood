eb6ea238c93633d1819c84c9b597b6b8c5130848
Anthony Martin <github@martin-studio.com>
2016-09-09 15:56:18 -0700

    better payout details

--- app/helpers/public_posts_helper.rb
+++ app/helpers/public_posts_helper.rb
@@ -8,11 +8,14 @@ module PublicPostsHelper
   end
   
   def public_post_detailed_payout(post)
-    [
-      "Total: #{post.content.total_payout_value}",
-      "Curator: #{post.content.curator_payout_value}",
-      "Pending: #{post.content.pending_payout_value}",
-      "Total Pending: #{post.content.total_pending_payout_value}"
-    ].join("; ").gsub(' SBD', '') + ' (SBD)'
+    payouts = []
+    c = post.content
+    
+    (v = c.total_payout_value) != '0.000 SBD' and payouts << "Author: #{v}"
+    (v = c.curator_payout_value) != '0.000 SBD' and payouts << "Curator: #{v}"
+    (v = c.pending_payout_value) != '0.000 SBD' and payouts << "Pending: #{v}"
+    (v = c.total_pending_payout_value) != '0.000 SBD' and payouts << "Total Pending: #{v}"
+      
+    payouts.join("; ").gsub(' SBD', '') + ' (SBD)'
   end
 end
