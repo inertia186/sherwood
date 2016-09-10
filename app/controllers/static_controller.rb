a6fac5c2c5b7709ed91becd56beda08a8a07e271
Anthony Martin <github@martin-studio.com>
2016-09-09 20:01:40 -0700

    posts reporting

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,6 @@ class StaticController < ApplicationController
   skip_before_action :authorize_user!, only: %w(default_project)
   
   def default_project
-    redirect_to public_posts_url(project_id: Project.first)
+    redirect_to public_posts_url(project_id: Project.first, sort_field: 'created_at', sort_order: 'desc')
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   def project_stats(project)
-    'TODO'
+    if project.posts.any?
+      "Latest post created #{time_ago_in_words project.posts.maximum(:created_at)} ago."
+    else
+      "No posts."
+    end
   end
   
   def sortable_header_link_to(name, field)
