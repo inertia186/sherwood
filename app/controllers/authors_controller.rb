8f5b34dcb6a58fcb2bbe4e46a4448c0693914a80
Anthony Martin <github@martin-studio.com>
2016-09-13 11:25:38 -0700

    better exclude logic

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,13 +3,15 @@ class AuthorsController < ApplicationController
   
   def index
     @project = Project.find params[:project_id]
-    @authors = @project.posts.created(@project.feature_duration_in_days.days.ago, false).
+    @excluded_authors = @project.posts.created(@project.feature_duration_in_days.days.ago).
       published.rejected(false).
       pluck(:steem_author)
+    @authors = @project.posts.published.rejected(false).
+      pluck(:steem_author) - @excluded_authors
     @latest_authors = @authors.select do |author|
       author_latest_post(@authors, author) > 24.hours.ago
     end.sort_by do |author|
-      author_latest_post_timestamp(@authors, author)
+      author_latest_post_timestamp(@authors, author).to_i
     end
     @oldest_authors = @authors.select do |author|
       author_latest_post(@authors, author) < 24.hours.ago
